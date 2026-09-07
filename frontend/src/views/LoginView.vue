254d1817d220faf19b49c771e79f21b76eb22fda
Magali Moreno <malali.moreno@gmail.com>
2026-09-07 07:50:48 -0300

    mejoras estilos logins

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,17 +3,17 @@
 
         <div class="login-container">
 
-            <h1>Libreria Suipacha</h1>
-            <h2>Iniciar Sesion</h2>
+            <h1 class="titulo-login">Libreria Suipacha</h1>
+            <h2 class="subtitulo-login">Iniciar Sesion</h2>
 
             <form>
                 <div class="formulario">
-                    <label>Usuario</label>
+                    <label class="label-login">Usuario</label>
                     <input type="text"v-model="usuario">
                 </div>
                 <div class="formulario">
 
-                    <label>Contraseña</label>
+                    <label class="lbel-login">Contraseña</label>
                     <input type="password" v-model="password">
                 </div>
 
@@ -53,10 +53,25 @@ const password = ref('')
     margin-bottom: 15px;
 }
 .btn-login {
-    width: 100%;
-    padding: 10px;
-    border: none;
-    border-radius: 6px;
-    cursor: pointer;
+    width: 100%; /*boton ocupa todo el ancho disponible*/
+    padding: 10px; /* agregfa espqcio interno y qumenta altur*/
+    border: none; /*elinina borfde predeterminado por el navegdor*/
+    border-radius: 6px; /*redonde esquinas*/
+    cursor: pointer; /*pasamos el mauose por eriba del bton y e[se conviete en icono mamo que puede clicker*/
+}
+.btn-login:hover {
+    opacity: 0.8; /* Hace que el botón sea un poco transparente al pasar el mouse */
+}
+.titulo-login {
+    text-align: center; /*Centra el texto dentro del espacio disponible:*/
+    margin-bottom: 10px; /*Agrega espacio por debajo del elemento.*/
+}
+
+.subtitulo-login {
+    text-align: center;
+    margin-bottom: 25px;
+}
+.label-login {
+    margin-bottom: 5px;
 }
 </style>
